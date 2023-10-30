713178ee4f32f259a57c88bf5e506325741ec4cc
Chris <chrismartinezlara@outlook.es>
2023-10-29 18:44:23 -0600

    First Commit

---
+++ src/main/java/dev/chrisjosue/calendarapi/CalendarApi.java
@@ -0,0 +1,13 @@
+package dev.chrisjosue.calendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarApi.class, args);
+    }
+
+}
